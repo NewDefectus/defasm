761fbd54aba030608833c72340d51c0488cba1b6
Alon Ran <alon.1.ran@gmail.com>
2021-02-21 16:40:02 +0200

    Add basic text editor

---
+++ script.js
@@ -0,0 +1,41 @@
+var asmTextInput = document.getElementById("inputArea");
+var asmTextOutput = document.getElementById("outputArea");
+
+var textEncoder = new TextEncoder();
+
+// Set the output
+function writeToOutput(bytes)
+{
+    asmTextOutput.textContent = [...bytes].reduce((a,x) =>
+        a + x.toString(16)
+        .toUpperCase()
+        .padStart(2, '0') + ' '
+    , "");
+}
+
+// Tabbing and newline editing help
+asmTextInput.onkeydown = function(event)
+{
+    if(event.keyCode == 9) // Tab
+    {
+        event.preventDefault();
+        if(this.selectionStart == this.selectionEnd)
+        {
+            document.execCommand("insertText", false, "\t");
+        }
+    }
+    else if(event.keyCode == 13) // Newline
+    {
+        event.preventDefault();
+        let prevLineStart = this.value.lastIndexOf("\n", this.selectionStart) + 1;
+        let prevLine = this.value.slice(prevLineStart, this.selectionStart);
+        prevLine = prevLine.split(/\S/, 1)[0]; // Get only the whitespaces at the start
+        document.execCommand("insertText", false, "\n" + prevLine)
+    }
+}
+
+// Input receiving
+asmTextInput.oninput = function(event)
+{
+    writeToOutput(textEncoder.encode(this.value))
+}
