7f8c7a7dc1feb6a0efbef63ac8d53860f3134898
NewDefectus <newdefectus@gmail.com>
2021-03-07 19:37:05 +0200

    Add codemirror editor

--- editor.js
+++ editor.js
@@ -1,29 +1,12 @@
-var asmTextInput = document.getElementById("inputArea");
+var editor = CodeMirror(document.getElementById("inputAreaContainer"), {
+    "theme": "editor",
+    "mode": {name: "gas", architecture: "x86"},
+    "lineNumbers": true
+});
 var asmTextOutput = document.getElementById("outputArea");
 
-// Tabbing and newline editing help
-asmTextInput.onkeydown = function(event)
-{
-    if(event.keyCode == 9) // Tab
-    {
-        event.preventDefault();
-        if(this.selectionStart == this.selectionEnd)
-        {
-            document.execCommand("insertText", false, "\t");
-        }
-    }
-    else if(event.keyCode == 13) // Newline
-    {
-        event.preventDefault();
-        let prevLineStart = this.value.lastIndexOf("\n", this.selectionStart) + 1;
-        let prevLine = this.value.slice(prevLineStart, this.selectionStart);
-        prevLine = prevLine.split(/\S/, 1)[0]; // Get only the whitespaces at the start
-        document.execCommand("insertText", false, "\n" + prevLine)
-    }
-}
-
 // Input receiving
-asmTextInput.oninput = function(event)
+editor.on("change", function()
 {
-    asmTextOutput.textContent = compileAsm(this.value);
-}
+    asmTextOutput.textContent = compileAsm(editor.getValue());
+})
