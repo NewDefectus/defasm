f3053fc76d6f75461bf177ca2dcb8fb800cf9eb5
NewDefectus <newdefectus@gmail.com>
2021-04-29 15:01:51 +0300

    Add parser files and codemirror dependencies

---
+++ codemirror/assembly.js
@@ -0,0 +1,29 @@
+import { asmPlugin, asmBytes }            from "../asmPlugin.js";
+import { parser }                         from "../parser.js";
+import { LezerLanguage, LanguageSupport } from '@codemirror/language';
+import { styleTags, tags }                from '@codemirror/highlight';
+
+export { asmBytes };
+
+const assemblyLang = LezerLanguage.define({
+    parser: parser.configure({
+        props: [
+            styleTags({
+                Opcode: tags.operatorKeyword,
+                Prefix: tags.keyword,
+                Register: tags.className,
+                Directive: tags.meta,
+                Comment: tags.lineComment,
+                LabelDefinition: tags.definition(tags.labelName),
+                MacroDefinition: tags.definition(tags.macroName),
+                Immediate: tags.literal,
+                Expression: tags.literal,
+                FullString: tags.string
+            })
+        ]
+    })
+});
+
+export function assembly() {
+    return new LanguageSupport(assemblyLang, asmPlugin);
+}
