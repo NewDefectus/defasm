b5bc97f3eca037aaca522dbab29e75b99fb31d4d
NewDefectus <newdefectus@gmail.com>
2021-05-05 19:37:52 +0300

    Add default code on gh page

--- gh-pages/editor.js
+++ gh-pages/editor.js
@@ -22,6 +22,57 @@ function getLastCode()
 {
     let prevCode = document.cookie.split('; ').find(row => row.startsWith("code="));
     if(prevCode)
-        return decodeURIComponent(prevCode.slice(5));
-    return "";
+        prevCode = decodeURIComponent(prevCode.slice(5));
+    return prevCode || `# Printing
+mov $1, %eax    # Syscall code 1 (write)
+mov $1, %edi    # File descriptor 1 (stdout)
+mov $text, %rsi # Address of buffer
+mov $14, %edx   # Length of buffer
+syscall
+
+# Looping
+mov $10, %bl
+numberLoop:
+    mov $1, %eax
+    mov $1, %edi
+    mov $digit, %rsi
+    mov $2, %edx
+    syscall
+
+    incb (%rsi)
+    dec %bl
+    jnz numberLoop
+
+# Accessing arguments
+pop %rbx
+pop %rax
+
+argLoop:
+    dec %rbx
+    jz endArgLoop
+
+    pop %rsi
+    mov %rsi, %rdi
+
+    mov $-1, %ecx
+    xor %al, %al
+    repnz scasb
+
+    not %ecx
+    movb $'\\n', -1(%rsi, %rcx)
+
+    mov %ecx, %edx
+    mov $1, %eax
+    mov $1, %edi
+    syscall
+
+    jmp argLoop
+endArgLoop:
+
+mov $60, %eax   # Syscall code 60 (exit)
+mov $0, %edi    # Exit code
+syscall
+
+text:  .string "Hello, World!\\n"
+digit: .byte   '0', '\\n'`;
 }
