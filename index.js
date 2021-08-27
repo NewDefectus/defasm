bb536021b015aef24be8df64639e7aa566befffc
NewDefectus <36775845+NewDefectus@users.noreply.github.com>
2021-08-27 01:38:23 +0000

    Deploying to gh-pages from @ NewDefectus/defAsm@1794ef4da4270f24de5d253e1ab7f6784a9e94e4 🚀

--- index.js
+++ index.js
@@ -11378,6 +11378,29 @@
   };
 
   // core/relocations.js
+  var relocTypes = {
+    NONE: 0,
+    64: 1,
+    PC32: 2,
+    GOT32: 3,
+    PLT32: 4,
+    COPY: 5,
+    GLOB_DAT: 6,
+    JUMP_SLOT: 7,
+    RELATIVE: 8,
+    GOTPCREL: 9,
+    32: 10,
+    33: 11,
+    16: 12,
+    PC16: 13,
+    8: 14,
+    PC8: 15,
+    PC64: 24,
+    GOTOFF64: 25,
+    GOTPC32: 26,
+    SIZE32: 32,
+    SIZE64: 33
+  };
   var signed32 = 33;
   var RelocEntry = class {
     constructor({ offset, addend, symbol, size, pcRelative, functionAddr }) {
@@ -11389,7 +11412,7 @@
         this.symbol = symbol.value.section.head.statement.record;
         this.addend += symbol.value.addend;
       }
-      this.type = (pcRelative ? functionAddr ? "PLT" : "PC" : "") + size;
+      this.type = relocTypes[(pcRelative ? functionAddr ? "PLT" : "PC" : "") + size];
     }
   };
 
