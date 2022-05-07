e4358bcaa91f5d947da6518680cc32847c18c20a
NewDefectus <newdefectus@gmail.com>
2022-05-07 15:39:57 +0300

    Add symedit test

---
+++ t/symedit.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+"use strict";
+// Editing of symbol names in expressions
+
+exports.run = async function()
+{
+    const { AssemblyState, Range } = await import("@defasm/core");
+    let state = new AssemblyState();
+    state.compile('str: .string "Hello, world!"\nstrLen = . - str', { haltOnError: true });
+    state.compile('', { range: new Range(state.source.length - 1, 1), haltOnError: true });
+    state.compile('r', { range: new Range(state.source.length - 1), haltOnError: true });
+}
+
+if(require.main === module)
+{
+    exports.run().then(x => process.exit(0)).catch(x => { console.error(x); process.exit(1) });
+}
