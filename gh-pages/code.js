36de8bde9cae3f02ebfa09516c32f00de0d40fb2
NewDefectus <newdefectus@gmail.com>
2024-03-23 17:38:22 +0200

    Redesign assembler page with hex output

--- gh-pages/code.js
+++ gh-pages/code.js
@@ -2,30 +2,60 @@ import { ASMStateField, ShellcodeField } from "@defasm/codemirror";
 
 const editorContainer = document.querySelector('.defasm-editor');
 
-const byteCount = document.getElementById('byteCount');
+const byteCount = document.getElementById('byte-count');
+const bytesSpan = document.getElementById('bytes-span');
+const bytesSection = document.getElementById('bytes-section');
 const shellcodeSpan = document.getElementById('shellcode');
+
+var selectedSection = null;
+
+bytesSection.onchange = () => {
+    if(bytesSection.selectedIndex === 0)
+        selectedSection = null;
+    else
+        selectedSection = bytesSection.value;
+    editorContainer.dispatch();
+}
+
+bytesSpan.onclick = () => {
+    let range = document.createRange();
+    range.setStart(bytesSpan, 0);
+    range.setEnd(bytesSpan, bytesSpan.childNodes.length);
+    document.getSelection().removeAllRanges();
+    document.getSelection().addRange(range);
+    document.execCommand('copy');
+}
+
+
+
+
+
 const urlParams = new URLSearchParams(window.location.search);
 const shellcodeEnabled = urlParams.has('shellcode');
 if(shellcodeEnabled)
 {
     let shellcodeContainer = document.getElementById('shellcodeContainer');
     shellcodeContainer.style.display = "block";
-    shellcodeContainer.onclick = () => {
-        let range = document.createRange();
-        range.setStart(shellcodeContainer, 0);
-        range.setEnd(shellcodeContainer, shellcodeContainer.childNodes.length);
-        document.getSelection().removeAllRanges();
-        document.getSelection().addRange(range);
-        document.execCommand('copy');
-    }
     editorContainer.setAttribute('shellcode', '');
 }
 
-editorContainer.dispatch = (tr, editor) => {
-    const result = editor.update([tr]);
-    const bytes = editor.state.field(ASMStateField).head.length();
-    document.cookie = "code=" + encodeURIComponent(tr.newDoc.sliceString(0)); // Save the code
+let globalEditor = null;
+editorContainer.dispatch = (tr = null, editor = globalEditor) => {
+    if(globalEditor === null)
+        globalEditor = editor;
+
+    let result = null;
+    if(tr !== null)
+    {
+        result = editor.update([tr]);
+        document.cookie = "code=" + encodeURIComponent(tr.newDoc.sliceString(0)); // Save the code
+    }
+
+    const state = editor.state.field(ASMStateField);
+    const shownHead = (selectedSection === null ? state : state.sections.find(x => x.name == selectedSection)).head;
+    const bytes = shownHead.length();
     byteCount.innerText = `${bytes} byte${bytes != 1 ? 's' : ''}`;
+    bytesSpan.innerText = [...shownHead.dump()].map(x => x.toString(16).toUpperCase().padStart(2, '0')).join(' ');
     if(shellcodeEnabled)
     {
         while(shellcodeSpan.hasChildNodes())
